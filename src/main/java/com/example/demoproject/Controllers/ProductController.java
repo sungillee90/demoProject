366bdf131b396bc6480a9e5da2f068f0175177cc
SUNG IL LEE <sungil.lee90@gmail.com>
2020-06-29 01:20:33 -0500

    feat: added anchortags for each products.

--- src/main/java/com/example/demoproject/Controllers/ProductController.java
+++ src/main/java/com/example/demoproject/Controllers/ProductController.java
@@ -58,13 +58,42 @@ public class ProductController {
     }
 
     @GetMapping("/product/{product_id}/edit")
-    public String editAProduct(@PathVariable long product_id) {
+    public String editAProductView(@PathVariable long product_id, Model model) {
+//        Product productToEdit = productDao.getOne(product_id);
+        model.addAttribute("product", productDao.getOne(product_id));
         return "product/edit";
+    }
+    @PostMapping("/product/{product_id}/edit")
+    public String editAProduct(
+            @PathVariable long product_id,
+            @RequestParam(name = "product_name") String productName,
+            @RequestParam(name = "product_description") String productDescription,
+            @RequestParam(name = "product_price") float productPrice,
+            @RequestParam(name = "product_img_url") String productImgUrl
+    ) {
+
+        Product productToEdit = new Product(product_id, productName, productDescription, productPrice, productImgUrl);
+        // put into DB so that I can grab the ID
+        productDao.save(productToEdit);
+
+        // return "product/create"; <-- returns VIEWs
+        return "redirect:/product/" + product_id;
     }
 
     @GetMapping("/product/{product_id}/delete")
-    public String deleteAProduct(@PathVariable long product_id) {
+    public String deleteAProductForm(@PathVariable long product_id, Model model) {
+
+        Product productToDelete = productDao.getOne(product_id);
+        model.addAttribute("product", productToDelete);
+
         return "product/delete";
+    }
+
+    @PostMapping("/product/{product_id}/delete")
+    public String deleteAProduct(@PathVariable long product_id) {
+        productDao.deleteById(product_id);
+
+        return "redirect:/products";
     }
 
 
